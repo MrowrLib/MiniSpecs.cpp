55fc1b40972b878af131f7d4779b9a271301be8d
Mrowr Purr <mrowr.purr@gmail.com>
2024-01-08 21:42:46 -0800

    Generate compile_commands

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 if not is_plat("windows") then
     set_languages("cxx11")
+    add_rules("plugin.compile_commands.autoupdate", { outputdir = "compile_commands" })
 end
 
 includes("*/xmake.lua")
