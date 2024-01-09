b854f2d0d0b11556810ae5ffe6f3de5fc1b9929d
Mrowr Purr <mrowr.purr@gmail.com>
2024-01-08 22:08:06 -0800

    Test macro OK for multiple tests (1 .cpp)

--- Examples/FirstTest.cpp
+++ Examples/FirstTest.cpp
@@ -1,16 +1,20 @@
 #include <MiniSpecs>
 
-#define Test(textDescription)                                                             \
-    void                         myTestFunction();                                        \
-    MiniSpecsCpp::FunctionRunner myRunner([]() {                                          \
-        MiniSpecsCpp::SpecRegistry::instance().add_test(textDescription, myTestFunction); \
-    });                                                                                   \
-    void                         myTestFunction()
+#define _MiniSpecs_Concat(a, b) a##b
+
+#define _MiniSpecs_TestDefinitionAndRegistration(textDescription, count)                    \
+    void                         _MiniSpecs_Concat(_MiniSpec_Test_, count)();               \
+    MiniSpecsCpp::FunctionRunner _MiniSpecs_Concat(_MiniSpec_FunctionRunner_, count)([]() { \
+        MiniSpecsCpp::SpecRegistry::instance().add_test(                                    \
+            textDescription, _MiniSpecs_Concat(_MiniSpec_Test_, count)                      \
+        );                                                                                  \
+    });                                                                                     \
+    void                         _MiniSpecs_Concat(_MiniSpec_Test_, count)()
+
+#define Test(textDescription) _MiniSpecs_TestDefinitionAndRegistration(textDescription, __COUNTER__)
 
 Test("Some cool test") {
     std::cout << "Hello, OMG we used a macro to define this test!" << std::endl;
 }
 
-// Test("second test") {
-
-// }
+Test("second test") { std::cout << "Hello, OMG Second!" << std::endl; }
