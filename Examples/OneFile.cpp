82f8d03cf763056beed071a4b528418acfac8f9a
Mrowr Purr <mrowr.purr@gmail.com>
2024-01-10 21:06:43 -0800

    Ideas 🤔

--- Examples/OneFile.cpp
+++ Examples/OneFile.cpp
@@ -3,17 +3,20 @@
 #include <MiniSpecs.h>
 #include <MiniSpecs/Main.h>
 
-Setup { std::cout << "Setup" << std::endl; }
-OneTimeSetup { std::cout << "OneTimeSetup" << std::endl; }
-OneTimeTeardown {
-    std::cout << "OneTimeTeardown" << std::endl;
-    throw "BOOM!";
-}
-OneTimeTeardown {
-    std::cout << "OneTimeTeardown" << std::endl;
-    throw "BOOM!";
-}
+// Idea:
+// TestData("First Values", 69, "String 69");
+// TestData("Second Values", 420, "String 420");
+// Test("Test 1") {
+//     //
+//     auto [str, num, text] = get_test_data<std::string, int, std::string>();
+//     auto justStr = get_test_data<std::string>(0);
+//     auto justNum = get_test_data<int>(1);
+// }
 
-Test("Test 1") {}
-Test("Test 2") {}
-Test("Test 3") {}
+// Or, another example that provides a list of {key, value} pairs for each TestData:
+// TestData({"key1", 69}, {"key2", "String 69"});
+// TestData({"key1", 420}, {"key2", "String 420"});
+// Test("Test 1") {
+//     //
+//    // Now you get the values using their keys:
+//    auto num = get_test_data<int>("key1");
